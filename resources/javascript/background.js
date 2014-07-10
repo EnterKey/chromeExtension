404a626d7a6ee199d43db20555b07b9d1499d062
bubae <widianpear@gmail.com>
2014-07-10 21:15:50 +0900

    bbu chrome extension

--- resources/javascript/background.js
+++ resources/javascript/background.js
@@ -1,29 +1,52 @@
-chrome.tabs.onUpdated.addListener(function(tabId, props) {
-	/*
-	 * to do..
-	 */
-});
+// chrome.tabs.onUpdated.addListener(function(tabId, props) {
+// 	/*
+// 	 * to do..
+// 	 */
+// });
 
-chrome.tabs.onActivated.addListener(function(activeInfo) {
-	var visitPageInfo = {
-		url : null,
-		title : null,
-		date : null,
-		visitedCnt : 1
-	}
+// chrome.tabs.onActivated.addListener(function(activeInfo) {
+// 	var visitPageInfo = {
+// 		url : null,
+// 		title : null,
+// 		date : null,
+// 		visitedCnt : 1
+// 	}
 	
-	chrome.tabs.get(activeInfo.tabId, function(tab) {
-		var url = tab.url, title = tab.title;
+// 	console.log('hello');
+// 	chrome.tabs.get(activeInfo.tabId, function(tab) {
+// 		var url = tab.url, title = tab.title;
 		
-		var visitedPageInfo = JSON.parse(window.localStorage.getItem(url));
-		if (visitedPageInfo != null) {
-			visitedPageInfo.visitedCnt += 1;
-			window.localStorage.setItem(visitedPageInfo.url, JSON.stringify(visitedPageInfo));
-		} else {
-			visitPageInfo.url = url;
-			visitPageInfo.title = title;
-			visitPageInfo.date = new Date();
-			window.localStorage.setItem(visitPageInfo.url, JSON.stringify(visitPageInfo));
-		}
-	});
+// 		var visitedPageInfo = JSON.parse(window.localStorage.getItem(url));
+// 		if (visitedPageInfo != null) {
+// 			visitedPageInfo.visitedCnt += 1;
+// 			window.localStorage.setItem(visitedPageInfo.url, JSON.stringify(visitedPageInfo));
+// 		} else {
+// 			visitPageInfo.url = url;
+// 			visitPageInfo.title = title;
+// 			visitPageInfo.date = new Date();
+// 			window.localStorage.setItem(visitPageInfo.url, JSON.stringify(visitPageInfo));
+// 		}
+// 	});
+// });
+
+chrome.tabs.onUpdated.addListener(function(tabId, changeInfo){
+	if (changeInfo.status === 'complete') {
+		chrome.tabs.get(tabId, function(tab){
+			var url = tab.url, title = tab.title;
+			var visitedPageInfo = JSON.parse(window.localStorage.getItem(url));
+
+			console.log(url, title, visitedPageInfo);
+			if (visitedPageInfo != null) {
+				visitedPageInfo.visitedCnt += 1;
+				window.localStorage.setItem(visitedPageInfo.url, JSON.stringify(visitedPageInfo));
+			} else {
+				visitedPageInfo = {};
+				visitedPageInfo.visitedCnt = 1;
+				visitedPageInfo.url = url;
+				visitedPageInfo.title = title;
+				visitedPageInfo.date = new Date();
+				window.localStorage.setItem(visitedPageInfo.url, JSON.stringify(visitedPageInfo));
+			}
+		});
+	}
 });
